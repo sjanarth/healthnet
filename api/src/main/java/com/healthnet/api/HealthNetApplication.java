d8ea82233046cb2b521cab1c60ceca58d24d02e1
sjanarth <sjanarth@gmail.com>
2021-01-02 19:18:53 -0800

    Switched to spring data rest

--- api/src/main/java/com/healthnet/api/HealthNetApplication.java
+++ api/src/main/java/com/healthnet/api/HealthNetApplication.java
@@ -2,9 +2,11 @@ package com.healthnet.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableCaching
 //@EnableJpaAuditing // sugarsaas enables this by default
+@EnableJpaRepositories({"com.healthnet.api"})
 @SpringBootApplication(scanBasePackages={"com.sugarsaas", "com.healthnet"})
 public class HealthNetApplication
 {
